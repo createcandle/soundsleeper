b6fc967610dc0d37d88fc9ffcdf2c751b97797f4
flatsiedatsie <unfold@gmail.com>
2025-05-28 08:14:51 +0200

    Update package.sh

--- package.sh
+++ package.sh
@@ -263,7 +263,9 @@ echo "PACKAGE.SH ALMOST THERE FOR PYTHON LIBS"
 echo "LS lib after second round of pip:"
 ls lib
 
-python3.11 -m pip install torch -t lib --no-cache-dir --prefix "" --global-option="-mfloat-abi=hard"
+# --config-settings
+
+python3.11 -m pip install torch torchvision numpy -t lib --prefix ""
 #python3.11 -m pip install numpy -t lib --no-cache-dir --no-binary :all: --prefix "" --global-option="-mfloat-abi=hard"
 python3.11 -m pip install -r requirements.txt -t lib --no-cache-dir --no-binary :all: --prefix ""
 
